e94d2c3ff4761d0e6f31907b3d7bfe0930765535
yesenia <yeseniab743@gmail.com>
2023-11-20 20:50:25 -0500

    added readme and started to create data in mysql

---
+++ assets/README.md
@@ -0,0 +1,2 @@
+## Employee Tracker
+### Description: The goal of thid spp is to use a node backend with inquirer to enable user management and other departmentsin the mysql database.
---
+++ assets/schema.sql
@@ -0,0 +1,9 @@
+drop database IF EXISTS employee_db;
+ create database employee_db;
+
+ USE employee_db;
+
+ create table table department (
+    id INT NOT NULL Auto_increment,
+    dept_name 
+ )
