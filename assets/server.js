1d570b3e2a1171c9c0418d3b71dab2256701476b
yesenia <yeseniab743@gmail.com>
2023-11-20 20:41:52 -0500

    added connection mysql

---
+++ assets/server.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection= mysql.createConnection({
+
+    host:'localhost',
+    port:3306
+    user: 'root'
+    password: 'docker123'
+    database:
+})
