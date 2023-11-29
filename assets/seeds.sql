8dc58244546429083c59c317515fef862142d5ca
yesenia <yeseniab743@gmail.com>
2023-11-29 05:50:38 -0500

    added Values to functionality

---
+++ assets/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO departments (name)
+VALUES ("Customer service")
+       ("Sales Department")
+       ("Booking Department")
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Customer care assoicate", 30000,1),
+       ("Sale agent", 50000,2),
+       ("Booking agent",45000,3),
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Sam", "Cuz",2),
+       ("Ashley", "Smith",1),
+       ("Bob", "Lee",3),
+       
