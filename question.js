8cfbc2c90403b79b52cc72921b6d5e81ca4f0652
yesenia <yeseniab743@gmail.com>
2023-12-10 11:11:46 -0500

    storing prompts

---
+++ question.js
@@ -0,0 +1,30 @@
+const i prompts = {
+    questions:[
+        {
+            type:'list',
+            name: 'start',
+            message: what would you like to do?
+
+            choices : [
+                'view all departments',
+                'view all employees',
+                'view all roles',
+                'view employees by manager',
+                'view employees by department',
+                'Add a department',
+                'add a role',
+                'add an employee',
+                'delete a department',
+                'delete an employee',
+                'delete a role',
+                'update an employee role',
+                'disconnect']
+
+        },
+        
+        {
+            
+        }
+    ]
+
+}
